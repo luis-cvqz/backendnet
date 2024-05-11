a983d44060ba33d38578458a60eb0b51da4a0c52
luis_cvqz <zs21013875@estudiantes.uv.mx>
2024-05-11 15:21:27 -0600

    Ajustes

--- Models/PeliculaDTO.cs
+++ Models/PeliculaDTO.cs
@@ -3,7 +3,7 @@ namespace backendnet.Models;
 public class PeliculaDTO
 {
     public int? PeliculaId { get; set; }
-    public string Titulo { get; set; } = "Sin titulo";
+    public required string Titulo { get; set; }
     public string Sinopsis { get; set; } = "Sin sinopsis";
     public int Anio { get; set; }
     public string Poster { get; set; } = "N/A";
