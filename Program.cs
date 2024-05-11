d24ed4cfd6eddeaaa083b115477df8b5d5265206
luis_cvqz <zs21013875@estudiantes.uv.mx>
2024-05-11 16:37:15 -0600

    Ejecucion del sistema

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using backendnet.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DataContext");
+
+builder.Services.AddDbContext<DataContext>(options => 
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3001", "https://localhost:8080")
+                .AllowAnyHeader().WithMethods("GET", "POST", "PUT", "DELETE");
+        });
+});
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.UseRouting();
+app.MapControllers();
 app.Run();
